3f31b8821f0d95845533a7314c50e01fde20b900
Satish <satishbunty053@gmail.com>
2026-01-16 23:18:31 +0530

    Added solution for adding and subtracting 2 matrices

---
+++ m03_matrices/p03_add_and_subtract_matrices.py
@@ -0,0 +1,63 @@
+"""
+Author      : Satish Kumar
+Created On  : 16/1/2026
+
+Problem Statement : Adding and Subtracting Matrices in Python
+
+Approaches:
+    1. Using numpy
+    2. Using nested loops
+
+Edge Cases to be considered:
+
+Time Complexity :
+
+Space Complexity :
+
+"""
+
+# Approach 1
+import numpy as np
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[4, 5], [6, 7]])
+
+print("Matrix A:\n", A)
+print("Matrix B:\n", B)
+
+# Adding matrices
+C = np.add(A, B)
+print("Addition Result:\n", C)
+
+# Adding matrices
+C1 = np.subtract(A, B)
+print("Addition Result:\n", C1)
+
+#Approach 2
+
+matrix1 = [[1, 2], [3, 4]]
+matrix2 = [[4, 5], [6, 7]]
+
+print("Matrix 1:")
+for row in matrix1:
+    print(row)
+
+print("Matrix 2:")
+for row in matrix2:
+    print(row)
+
+add_result = [[0, 0], [0, 0]]
+sub_result = [[0, 0], [0, 0]]
+
+# Perform addition and subtraction
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        add_result[i][j] = matrix1[i][j] + matrix2[i][j]
+        sub_result[i][j] = matrix1[i][j] - matrix2[i][j]
+
+print("Addition Result:")
+for row in add_result:
+    print(row)
+
+print("Subtraction Result:")
+for row in sub_result:
+    print(row)
